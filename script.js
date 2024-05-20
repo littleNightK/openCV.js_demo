dfde86c4b8f0f948721bcd5684197e8dde4db989
littleNightK <khoindm.bi12-218@st.usth.edu.vn>
2024-05-20 14:02:18 +0700

    IOS version 3 test

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 const scanner = new jscanify();
-const canvas = document.getElementById('canvas');
-const result = document.getElementById('result');
-const video = document.getElementById('video');
+const canvas = document.getElementById("canvas");
+const result = document.getElementById("result");
+const video = document.getElementById("video");
 
-navigator.mediaDevices.getUserMedia({ 
-  video: { 
-    facingMode: { ideal: "environment" } 
-  } 
-}).then((stream) => {
+navigator.mediaDevices
+  .getUserMedia({
+    video: {
+      facingMode: { ideal: "environment" },
+    },
+  })
+  .then((stream) => {
     video.srcObject = stream;
     video.muted = true; // Ensure the video is muted
-    video.play();
-  
-  video.onplay = () => {
-    // Set canvas dimensions to match video, but scale down if necessary
-    const maxDimension = Math.min(video.videoWidth, video.videoHeight, 1024); // iOS limit
-    const aspectRatio = video.videoWidth / video.videoHeight;
-    if (video.videoWidth > video.videoHeight) {
-      canvas.width = maxDimension;
-      canvas.height = maxDimension / aspectRatio;
-    } else {
-      canvas.width = maxDimension * aspectRatio;
-      canvas.height = maxDimension;
-    }
-    result.width = canvas.width;
-    result.height = canvas.height;
+    video.onloadedmetadata = () => {
+      video.play();
+    };
 
-    const canvasCtx = canvas.getContext("2d");
-    const resultCtx = result.getContext("2d");
+    video.onplay = () => {
+      // Set canvas dimensions to match video, but scale down if necessary
+      const maxDimension = Math.min(video.videoWidth, video.videoHeight, 1024); // iOS limit
+      const aspectRatio = video.videoWidth / video.videoHeight;
+      if (video.videoWidth > video.videoHeight) {
+        canvas.width = maxDimension;
+        canvas.height = maxDimension / aspectRatio;
+      } else {
+        canvas.width = maxDimension * aspectRatio;
+        canvas.height = maxDimension;
+      }
+      result.width = canvas.width;
+      result.height = canvas.height;
 
-    setInterval(() => {
-      canvasCtx.drawImage(video, 0, 0, canvas.width, canvas.height);
-      const resultCanvas = scanner.highlightPaper(canvas);
-      resultCtx.drawImage(resultCanvas, 0, 0, result.width, result.height);
-    }, 10);
-  };
-});
+      const canvasCtx = canvas.getContext("2d");
+      const resultCtx = result.getContext("2d");
+
+      setInterval(() => {
+        canvasCtx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const resultCanvas = scanner.highlightPaper(canvas);
+        resultCtx.drawImage(resultCanvas, 0, 0, result.width, result.height);
+      }, 10);
+    };
+  });
