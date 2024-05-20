a61888277ce12b4ae50d3a63b742c8c31b81859c
littleNightK <khoindm.bi12-218@st.usth.edu.vn>
2024-05-20 13:21:34 +0700

    version 3 test

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const scanner = new jscanify();
-const canvasCtx = canvas.getContext("2d");
-const resultCtx = result.getContext("2d");
+const canvas = document.getElementById('canvas');
+const result = document.getElementById('result');
+const video = document.getElementById('video');
+
 navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
   video.srcObject = stream;
   video.onloadedmetadata = () => {
     video.play();
 
+    // Set canvas dimensions to match video
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    result.width = video.videoWidth;
+    result.height = video.videoHeight;
+
+    const canvasCtx = canvas.getContext("2d");
+    const resultCtx = result.getContext("2d");
+
     setInterval(() => {
-      canvasCtx.drawImage(video, 0, 0);
+      canvasCtx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
       const resultCanvas = scanner.highlightPaper(canvas);
-      resultCtx.drawImage(resultCanvas, 0, 0);
+      resultCtx.drawImage(resultCanvas, 0, 0, video.videoWidth, video.videoHeight);
     }, 10);
   };
 });
